d112c9421a74201f29fa830b61e614a961d63fdd
rana jabbar <rfayed8@hotmail.com>
2024-04-08 15:52:45 +0100

    started library page

--- application/routes.py
+++ application/routes.py
@@ -5,4 +5,10 @@ from application import app
 @app.route('/')
 @app.route('/home/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',title='home')
+
+@app.route('/my_library')
+def my_library():
+    return render_template('my_library.html', title='my_library')
+
+
