897e9b15b89c797ade3ca5be8f1809002098d489
Jeanette <khojeanette@gmail.com>
2024-04-09 16:59:52 +0100

    Working library with books from database

--- application/routes.py
+++ application/routes.py
@@ -48,7 +48,8 @@ def logout():
 @app.route('/my_library')
 def my_library():
     username = session.get('username')
-    return render_template('my_library.html', title='my_library', username=username)
+    books_from_db = get_all_books()
+    return render_template('my_library.html', title='my_library', username=username, books_from_db=books_from_db)
 
 @app.route('/my_books')
 def my_books():
