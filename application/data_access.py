542e327bd2db34fd20fdda46071dd5ed5b0be755
Anagha <mailsforanagha@gmail.com>
2024-04-13 06:10:51 +0100

    added error handling in data_access.py and routes.py

--- application/data_access.py
+++ application/data_access.py
@@ -3,13 +3,17 @@ import mysql.connector
 
 
 def get_db_connection():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        database="book_tracker",
-        password="Pa$$w0rd" if sys.platform == 'win32' else ""
-    )
-    return mydb
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="book_tracker",
+            password="Pa$$w0rd" if sys.platform == 'win32' else ""
+        )
+        return mydb
+    except mysql.connector.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        return None
 
 
 def get_user(username, password):
