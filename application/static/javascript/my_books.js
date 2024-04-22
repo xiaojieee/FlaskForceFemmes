fd868ca4b6ee4fa7f99026c081843914f1fa2e87
Jeanette <khojeanette@gmail.com>
2024-04-22 11:36:16 +0100

    Fixed My Books styles and created 2nd popup, but not yet edited except button

--- application/static/javascript/my_books.js
+++ application/static/javascript/my_books.js
@@ -1,3 +1,4 @@
+// POP UP | RECOMMENDED BOOKS
 function openPopup(title, imageUrl, author, blurb, genre, pages, readingLevel, book_id, isSavedBook=false) {
     console.log("isSavedBook:", isSavedBook);
     const popupOverlay = document.getElementById("popupOverlay");
@@ -9,7 +10,7 @@ function openPopup(title, imageUrl, author, blurb, genre, pages, readingLevel, b
     const popupPages = document.getElementById("popupPages");
     const popupReadingLevel = document.getElementById("popupReadingLevel");
     const addToReadingListBtn = document.getElementById("addToReadinglist");
-    addToReadingListBtn.style.display = isSavedBook ? 'none' : 'block';
+//    addToReadingListBtn.style.display = isSavedBook ? 'none' : 'block';
 //    if (isSavedBook){
 //    document.getElementById("addToReadinglist").style.display = 'none';
 //    }
@@ -40,6 +41,45 @@ function openPopup(title, imageUrl, author, blurb, genre, pages, readingLevel, b
     });
   });
 
+
+// POP UP | SAVED BOOKS & FINISHED BOOKS
+  function openPopupTwo(title, imageUrl, author, blurb, genre, pages, readingLevel, book_id, isSavedBook=false) {
+    console.log("isSavedBook:", isSavedBook);
+    const popupOverlay = document.getElementById("popupOverlayTwo");
+    const popupTitle = document.getElementById("popupTitleTwo");
+    const popupImage = document.getElementById("popupImageTwo");
+    const popupAuthor = document.getElementById("popupAuthorTwo");
+    const popupBlurb = document.getElementById("popupBlurbTwo");
+    const popupGenre = document.getElementById("popupGenreTwo");
+    const popupPages = document.getElementById("popupPagesTwo");
+    const popupReadingLevel = document.getElementById("popupReadingLevelTwo");
+    const removeFromReadingListBtn = document.getElementById("removeFromReadinglist");
+
+    removeFromReadingListBtn.addEventListener("click", function () {
+
+    });
+
+    popupOverlay.style.display = "block";
+    popupTitle.textContent = title;
+    popupImage.src = imageUrl;
+    popupAuthor.textContent = 'Author: ' + author;
+    popupBlurb.textContent = 'About: ' + blurb;
+    popupGenre.textContent = 'Genre: ' + genre;
+    popupPages.textContent = 'Pages: ' + pages;
+    popupReadingLevel.textContent = 'Reading Level: ' + readingLevel;
+
+  }
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const closePopupBtn = document.getElementById("closePopupTwo");
+    const popupOverlay = document.getElementById("popupOverlayTwo");
+    closePopupBtn.addEventListener("click", function () {
+      popupOverlay.style.display = "none";
+    });
+  });
+
+
+// ADDS BOOKS TO MY BOOKS PAGE
   function activateAddToButton(book_id)
   {
     const username = '{{session.username}}';
